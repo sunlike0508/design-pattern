ebb52fe05b9dedfd1bf77e1dfb05f58b07538d9a
seonhoshin <sunlike0508@waug.com>
2024-08-27 13:15:56 +0900

    flyweight 패턴

---
+++ src/flyweight/after/Font.java
@@ -0,0 +1,12 @@
+package flyweight.after;
+
+public class Font {
+    private String fontFamily;
+    private int fontSize;
+
+
+    public Font(String fontFamily, int fontSize) {
+        this.fontFamily = fontFamily;
+        this.fontSize = fontSize;
+    }
+}
